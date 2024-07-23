d7d47e89cde4d96a1a3ae113ff6d628440614ec9
Alex KooWeso <koowa360@gmail.com>
2024-07-23 05:21:05 +0300

    ready

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['vite.config.ts'],
+        optionalDependencies: ['vite.config.ts'],
+        peerDependencies: ['vite.config.ts'],
+      },
+    ],
+  },
+};
